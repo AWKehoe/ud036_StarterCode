0f02f1bcb46f25f9abe861f17343d2ba0f357eaf
AWKehoe <kehoto@yahoo.co.uk>
2017-01-17 09:03:05 +0000

    Draft working version

--- entertainment.py
+++ entertainment.py
@@ -1,8 +1,19 @@
+import fresh_tomatoes
 from media import Movie
 
-toy_story = Movie("Toy Story","Animated movie about toys")
+# Create instances of Movies
 
-print(toy_story.storyline)
-print(Movie.__module__)
-print(Movie.__name__)
-print(Movie.__doc__)
+toy_story_4 = Movie("Toy Story 4", 
+                    "Animated movie about crazy toys", 
+                    "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                    "https://www.youtube.com/watch?v=QW0sjQFpXTU")
+
+avatar = Movie("Avatar", 
+               "Animated fantasy movie about some weird blue people",
+               "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+               "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+movies = [toy_story_4, avatar]
+
+fresh_tomatoes.open_movies_page(movies)
+
--- media.py
+++ media.py
@@ -2,13 +2,12 @@ class Movie():
     """ A class that defines movies """
     movie_ratings = ['U', 'PG', '13', 'R']
     
-    def __init__(self, movie_title, movie_storyline, movie_trailer_url, movie_poster_image, movie_genre, movie_duration):
+    def __init__(self, movie_title, movie_storyline, movie_poster_image, movie_trailer_url):
         self.title = movie_title
         self.storyline = movie_storyline
-        self.trailerurl = movie_trailer_url
-        self.posterimage = movie_poster_image        
-        self.genre = movie_genre
-        self.duration = movie_duration
+        self.poster_image_url = movie_poster_image
+        self.trailer_youtube_url = movie_trailer_url
+    
         
 
         
