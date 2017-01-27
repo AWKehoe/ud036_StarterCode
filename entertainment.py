1b61f69366caf46f455e4362cab5b9be1ddd3545
Anthony Kehoe <anthonykehoe@Anthonys-MacBook.local>
2017-01-27 07:45:46 +0000

    Updated doc sttring in movie class and added comments

--- entertainment.py
+++ entertainment.py
@@ -1,19 +1,41 @@
 import fresh_tomatoes
 from media import Movie
 
-# Create instances of Movies
+# Create instances of my favourite War Movies using Movie Class
 
-toy_story_4 = Movie("Toy Story 4", 
-                    "Animated movie about crazy toys", 
-                    "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                    "https://www.youtube.com/watch?v=QW0sjQFpXTU")
+greatescape = Movie("The Great Escape", 
+                    "Steve McQueen fails to escape POW camp",
+                    "https://upload.wikimedia.org/wikipedia/en/c/c7/Great_escape.jpg",
+                    "https://youtu.be/hBpp1OTq0rY")
 
-avatar = Movie("Avatar", 
-               "Animated fantasy movie about some weird blue people",
-               "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
-               "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+bridgetofar = Movie("A Bride To Far", 
+                    "The story of Operation Market Garden",
+                    "https://upload.wikimedia.org/wikipedia/en/e/e7/Bridge_too_far_movieposter.jpg",
+                    "https://youtu.be/AWL184ZcSxA")
 
-movies = [toy_story_4, avatar]
+battleofbritain = Movie("The Battle Of Britain", 
+                    "How the RAF defeated the might of the German Lufwaffe",
+                    "https://upload.wikimedia.org/wikipedia/en/1/12/Battle_of_Britain_%28movie_poster%29.jpg",
+                    "https://youtu.be/KwBcnbutpjw")
 
+battleofthebulge = Movie("Battle Of The Bulge", 
+                        "Not a story of struggles of dieting...",
+                        "https://upload.wikimedia.org/wikipedia/en/5/5b/Bulge_sheet_A.jpg",
+                        "https://youtu.be/mdjau_yi5AY")
+
+platoon = Movie("Platoon", 
+                         "One mans fight for the honor of war",
+                        "https://upload.wikimedia.org/wikipedia/en/a/a9/Platoon_posters_86.jpg",
+                        "https://youtu.be/hGsyEkfjhQk")
+
+metaljacket = Movie("Full Metal Jacket", 
+                        "Story of a soldier who wears a metal jacket",
+                        "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg",
+                        "https://youtu.be/x9f6JaaX7Wg")
+
+# Create a list of movies 
+movies = [greatescape, bridgetofar, battleofbritain, battleofthebulge, platoon, metaljacket]
+
+# Module that creates a HTML file which visualizes all of your favorite movies.
 fresh_tomatoes.open_movies_page(movies)
 
--- media.py
+++ media.py
@@ -1,12 +1,20 @@
-class Movie():
-    """ A class that defines movies """
-    movie_ratings = ['U', 'PG', '13', 'R']
+class Movie(object):
+    """ A Movie class that describes information related to title, links to poster image and link to trailer
+    
+    Attributes:
+        title: A string that contains the movie title
+        storyline: A string to hold a brief storyline
+        poster_image_url: A string describing the url to a poster image for the movie
+        trailer_youtube_url: A string describing the url for the movie trailer
+    """
     
     def __init__(self, movie_title, movie_storyline, movie_poster_image, movie_trailer_url):
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = movie_poster_image
-        self.trailer_youtube_url = movie_trailer_url
+        """Initiate Movie instance with title, storyline, poster image and movie trailer"""
+        
+        self.title = movie_title  # Text to hold film title
+        self.storyline = movie_storyline  # Strapline of the film story
+        self.poster_image_url = movie_poster_image  # A url to a poster image
+        self.trailer_youtube_url = movie_trailer_url  # A url to the film trailer
     
         
 
